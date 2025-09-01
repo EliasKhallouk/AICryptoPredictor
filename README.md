9c571eb5eabb89cab810f2ed43be5e020b2e921a
EliasKhallouk <eliaskhallouk@gmail.com>
2025-09-01 17:26:03 +0200

    Automating Dataset Retrieval

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # AICryptoPredictor
 
 date starting : 30/08/2025
-last update : 31/08/2025
+last update : 01/09/2025
+
+### File : crontabe -e
+00 */1 * * * /home/elias/anaconda3/envs/crypto_predictor/bin/python /home/elias/PROJECT/AICryptoPredictor/notebooks/update_data.py >> /home/elias/PROJECT/AICryptoPredictor/Output/cron_update.log 2>&1
--- notebooks/update_data.py
+++ notebooks/update_data.py
@@ -4,14 +4,14 @@ from datetime import datetime
 
 # Nom du dataset Kaggle
 DATASET = "mczielinski/bitcoin-historical-data"
-LOCAL_PATH = "./PROJECT/AICryptoPredictor/data/btcusd_1-min_data.csv"
+LOCAL_PATH = "./PROJECT/AICryptoPredictor/data"
 
 def update_dataset():
     print(f"[{datetime.now()}] Téléchargement du dataset...")
     # Télécharge uniquement si il y a une nouvelle version
     subprocess.run([
-        "kaggle", "datasets", "download", "-d", DATASET,
-        "-p", "data", "--unzip", "--force"
+        "/home/elias/anaconda3/envs/crypto_predictor/bin/kaggle", "datasets", "download", "-d", DATASET,
+        "-p", LOCAL_PATH, "--unzip", "--force"
     ])
     print("✅ Dataset mis à jour dans /data")
 
